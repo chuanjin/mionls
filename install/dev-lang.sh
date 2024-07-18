b61414ce1631c912673e956668ffdd657d379104
cj <chuan.jin.813@gmail.com>
2024-07-18 20:17:35 +0200

    Add languages

---
+++ install/dev-lang.sh
@@ -0,0 +1,35 @@
+curl https://mise.run | sh
+
+AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "Python" "Elixir" "Rust" "Java")
+languages=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --height 10 --header "Select programming languages")
+
+if [[ -n "$languages" ]]; then
+	for language in $languages; do
+		case $language in
+		Ruby)
+			mise use --global ruby@3.3
+			mise x ruby -- gem install rails --no-document
+			;;
+		Node.js)
+			mise use --global node@lts
+			;;
+		Go)
+			mise use --global go@latest
+			;;
+		Python)
+			mise use --global python@latest
+			;;
+		Elixir)
+			mise use --global erlang@latest
+			mise use --global elixir@latest
+			mise x elixir -- mix local.hex --force
+			;;
+		Rust)
+			bash -c "$(curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs)" -- -y
+			;;
+		Java)
+			mise use --global java@latest
+			;;
+		esac
+	done
+fi
