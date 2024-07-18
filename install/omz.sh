9c3512fe45627368d7ada4e9e2840d275fc9a856
cj <chuan.jin.813@gmail.com>
2024-07-18 23:22:36 +0200

    Add plugin

--- install/omz.sh
+++ install/omz.sh
@@ -1,5 +1,7 @@
 echo no | sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
 # Install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
