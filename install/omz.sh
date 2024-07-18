cb8c693690ead13ede609977e5e1fe5345db0255
cj <chuan.jin.813@gmail.com>
2024-07-18 22:46:30 +0200

    Add install DBs

--- install/omz.sh
+++ install/omz.sh
@@ -1,6 +1,7 @@
-sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+echo no | sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-zsh -c "
-  [ -f "~/.zshrc" ] && mv ~/.zshrc ~/.zshrc.bak
-  cp ~/.local/share/${project}/configs/zshrc ~/.zshrc
-"
+# Install powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+[ -f "~/.zshrc" ] && mv ~/.zshrc ~/.zshrc.bak
+cp ~/.local/share/${project}/configs/zshrc ~/.zshrc
