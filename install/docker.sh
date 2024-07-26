24a5fef1b2628c452c87a0730f7fe42dba888f7c
cj <chuan.jin.813@gmail.com>
2024-07-26 11:01:16 +0200

    Add optional install foler

--- install/docker.sh
+++ install/docker.sh
@@ -1,3 +1,4 @@
 sudo pacman -S --quiet --noconfirm docker
 sudo usermod -aG docker "${USER}"
 sudo systemctl start docker
+sudo systemctl enable docker
--- install/omz.sh
+++ install/omz.sh
@@ -1,8 +1,6 @@
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo no | sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 	git clone "https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
-	# Install powerlevel10k
-	git clone --depth=1 "https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 else
 	[ -f "$HOME/.zshrc" ] && mv ~/.zshrc ~/.zshrc.bak
 	cp ~/.local/share/${project}/configs/zshrc ~/.zshrc
