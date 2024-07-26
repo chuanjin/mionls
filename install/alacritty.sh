a8ad9eae14f6656b0bd1be1b2730e6c0fe138393
cj <chuan.jin.813@gmail.com>
2024-07-26 22:20:01 +0200

    Update alacritty

--- install/alacritty.sh
+++ install/alacritty.sh
@@ -3,5 +3,5 @@ sudo pacman -S alacritty
 mkdir -p ~/.config/alacritty
 cp ~/.local/share/${project}/configs/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp ~/.local/share/${project}/themes/tokyo-night/alacritty.toml ~/.config/alacritty/theme.toml
-cp ~/.local/share/${project}/configs/alacritty/fonts/CaskaydiaMono.toml ~/.config/alacritty/font.toml
+cp ~/.local/share/${project}/configs/alacritty/fonts/SauceCodePro.toml ~/.config/alacritty/font.toml
 cp ~/.local/share/${project}/configs/alacritty/font-size.toml ~/.config/alacritty/font-size.toml
