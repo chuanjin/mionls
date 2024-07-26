94d753c48bce2b74f30dc0c6d841212bffc86618
cj <chuan.jin.813@gmail.com>
2024-07-26 13:13:47 +0200

    Fix double quote

--- install/omz.sh
+++ install/omz.sh
@@ -1,6 +1,6 @@
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
 	echo no | sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-	git clone "https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+	git clone "https://github.com/zsh-users/zsh-autosuggestions" ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 else
 	[ -f "$HOME/.zshrc" ] && mv ~/.zshrc ~/.zshrc.bak
 	cp ~/.local/share/${project}/configs/zshrc ~/.zshrc
