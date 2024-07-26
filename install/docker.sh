fe4587b2694da5d96a97cfc0f486e1bc5311d55a
cj <chuan.jin.813@gmail.com>
2024-07-26 11:39:06 +0200

    Add more packages

--- install/docker.sh
+++ install/docker.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --quiet --noconfirm docker
+sudo pacman -S --quiet --noconfirm docker docker-buildx
 sudo usermod -aG docker "${USER}"
 sudo systemctl start docker
 sudo systemctl enable docker
