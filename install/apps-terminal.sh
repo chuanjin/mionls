60e52dbc9eb699837669c93f563e67d89ac7cda2
cj <chuan.jin.813@gmail.com>
2024-07-31 17:58:34 +0200

    Add delta

--- install/apps-terminal.sh
+++ install/apps-terminal.sh
@@ -1,1 +1,1 @@
-sudo pacman -S --quiet --needed git base-devel yay fish zsh fzf ripgrep bat eza zoxide btop fd gum fastfetch lazygit gsimplecal rofi ranger polybar
+sudo pacman -S --quiet --needed git base-devel yay fish zsh fzf ripgrep bat eza zoxide btop fd gum fastfetch lazygit git-delta gsimplecal rofi ranger polybar
---
+++ install/delta.sh
@@ -0,0 +1,2 @@
+mv ~/.gitconfig{,.bak}
+cp ~/.local/share/${project}/configs/.gitconfig ~/.gitconfig
