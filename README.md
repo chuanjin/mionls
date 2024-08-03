480dfef5d930417967e4077216ba0080a482c7a5
cj <chuan.jin.813@gmail.com>
2024-08-03 20:54:19 +0200

    Update readme file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 Inspired by [omakub](https://github.com/basecamp/omakub), but for Manjaro i3.
+
+## Install
+
+```bash
+source boot.sh
+```
--- install/i3.sh
+++ install/i3.sh
@@ -1,3 +1,4 @@
+mkdir -p ~/.i3
 cp ~/.local/share/${project}/configs/i3/config ~/.i3/config
 mkdir -p ~/.config/i3status/
 cp ~/.local/share/${project}/configs/i3/i3status ~/.config/i3status/config
