9a2e75741bb6a4faf46580d3d286db371620766c
cj <chuan.jin.813@gmail.com>
2024-11-04 07:39:15 +0100

    Add set DPI to adjust elements sizes based on nbr of monitors.
    To scale high resolution and have interface elements sizes
    adjusted automatically based on the number of monitors detected.

---
+++ configs/i3/set_dpi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Count the number of connected monitors
+monitor_count=$(xrandr --query | grep " connected" | wc -l)
+
+# Set DPI based on the monitor count
+if [ "$monitor_count" -eq 1 ]; then
+  dpi=96
+else
+  dpi=132
+fi
+
+# Update or add the Xft.dpi line in .Xresources
+if grep -q "Xft.dpi" ~/.Xresources; then
+  # If Xft.dpi exists, replace it
+  sed -i "s/^Xft.dpi:.*/Xft.dpi: $dpi/" ~/.Xresources
+else
+  # If Xft.dpi does not exist, add it
+  echo "Xft.dpi: $dpi" >>~/.Xresources
+fi
+
+# Apply the updated DPI setting
+xrdb -merge ~/.Xresources
--- install/i3.sh
+++ install/i3.sh
@@ -1,4 +1,5 @@
 mkdir -p ~/.i3
 cp ~/.local/share/${project}/configs/i3/config ~/.i3/config
+cp ~/.local/share/${project}/configs/i3/set_dpi.sh ~/.i3/set_dpi.sh
 mkdir -p ~/.config/i3status/
 cp ~/.local/share/${project}/configs/i3/i3status ~/.config/i3status/config
