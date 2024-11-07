f181b71c36c784cd5d713b7144eefef2d31b806d
cj <chuan.jin.813@gmail.com>
2024-11-07 21:50:50 +0100

    Add update script

---
+++ update.sh
@@ -0,0 +1,16 @@
+set -e
+
+export project="mionls"
+
+sudo pacman -Syu
+
+cd ~/.local/share/${project}
+git stash && git pull origin main
+
+AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "Python" "Elixir" "Rust" "Java")
+SELECTED_LANGUAGES="Go","Node.js","Python"
+export FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
+
+for installer in ~/.local/share/${project}/install/*.sh; do source $installer; done
+
+echo "Update Completed!"
