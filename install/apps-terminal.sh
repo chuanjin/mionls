4bf693add446a680011d7f27fb7758073078f736
cj <chuan.jin.813@gmail.com>
2024-11-13 17:19:48 +0100

    Remove duplicated package

--- install/apps-terminal.sh
+++ install/apps-terminal.sh
@@ -1,4 +1,4 @@
-sudo pacman -S --quiet --needed git base-devel yay fish zsh fzf ripgrep bat eza zoxide btop fd gum fastfetch lazygit git-delta gsimplecal rofi ranger polybar
+sudo pacman -S --quiet --needed base-devel yay fish zsh fzf ripgrep bat eza zoxide btop fd gum fastfetch lazygit git-delta gsimplecal rofi ranger polybar
 sudo pacman -S fcitx5-im fcitx5-chinese-addons
 
 sudo pacman -S pulseaudio pavucontrol
