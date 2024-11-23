da3725efbe0486a951d810a61f09b84665da7b87
cj <chuan.jin.813@gmail.com>
2024-11-23 19:57:16 +0100

    Copy setup monitor script to target folder

--- install/i3.sh
+++ install/i3.sh
@@ -1,5 +1,6 @@
 mkdir -p ~/.i3
 cp ~/.local/share/${project}/configs/i3/config ~/.i3/config
 cp ~/.local/share/${project}/configs/i3/set_dpi.sh ~/.i3/set_dpi.sh
+cp ~/.local/share/${project}/configs/i3/setup_monitor.sh ~/.i3/setup_monitor.sh
 mkdir -p ~/.config/i3status/
 cp ~/.local/share/${project}/configs/i3/i3status ~/.config/i3status/config
