d31b6dcf422f6f6676abb2e89cc62c040b70c856
cj <chuan.jin.813@gmail.com>
2024-11-30 20:32:10 +0100

    Add DB section in update script

--- update.sh
+++ update.sh
@@ -12,6 +12,10 @@ AVAILABLE_LANGUAGES=("Ruby on Rails" "Node.js" "Go" "Python" "Elixir" "Rust" "Ja
 SELECTED_LANGUAGES="Go","Node.js","Python"
 export FIRST_RUN_LANGUAGES=$(gum choose "${AVAILABLE_LANGUAGES[@]}" --no-limit --selected "$SELECTED_LANGUAGES" --height 10 --header "Select programming languages")
 
+AVAILABLE_DBS=("MySQL" "Redis" "PostgreSQL")
+SELECTED_DBS="PostgreSQL,Redis"
+export FIRST_RUN_DBS=$(gum choose "${AVAILABLE_DBS[@]}" --no-limit --selected "$SELECTED_DBS" --height 5 --header "Select databases (runs in Docker)")
+
 for installer in ~/.local/share/${project}/install/*.sh; do source $installer; done
 
 echo "Update Completed!"
